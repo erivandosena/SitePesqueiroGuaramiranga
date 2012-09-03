204ef2b82029a6126b7b9991b8bf878bbce67155
erivandoramos <erivandoramos@19eec290-83bd-4d1e-926f-9a380578fa76>
2012-09-03 18:08:43 +0000

    Configurado o CRUD e a interface de visão.

--- trunk/src/main/java/br/net/rwd/website/dao/SiteDAO.java
+++ trunk/src/main/java/br/net/rwd/website/dao/SiteDAO.java
@@ -1,67 +1,10 @@
 package br.net.rwd.website.dao;
 
-import java.util.List;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
+import java.io.Serializable;
 
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import br.net.rwd.website.entidade.Site;
 
 @Repository("siteDao")
-public class SiteDAO {
+public class SiteDAO extends DAOGenerico<Serializable> {
 
-	@PersistenceContext
-	protected EntityManager entityManager;
-
-	@Transactional(readOnly = true)
-	public List<Site> findAll() {
-
-		String jpql = " SELECT s from Site s order by s.web_titulo";
-		Query query = entityManager.createQuery(jpql);
-
-		@SuppressWarnings("unchecked")
-		List<Site> sites = (List<Site>) query.getResultList();
-		return sites;
-	}
-
-	@SuppressWarnings("unchecked")
-	@Transactional(readOnly = true)
-	public List<Site> findByName(String tituloSite) {
-
-		String jpql = " SELECT s from Site s where web_titulo like :titulo order by s.web_titulo";
-		Query query = entityManager.createQuery(jpql);
-		query.setParameter("titulo", "%" + tituloSite + "%");
-
-		List<Site> sites = (List<Site>) query.getResultList();
-		return sites;
-
-	}
-
-	@Transactional
-	public Site create(Site site) {
-		entityManager.persist(site);
-		return site;
-
-	}
-
-	@Transactional
-	public void update(Site site) {
-		entityManager.merge(site);
-	}
-
-	@Transactional
-	public void delete(Site site) {
-
-		site = entityManager.find(Site.class, site.getWeb_cod());
-		entityManager.remove(site);
-
-	}
-
-	public void setEntityManager(EntityManager entityManager) {
-		this.entityManager = entityManager;
-	}
 }
--- trunk/src/main/java/br/net/rwd/website/servico/SiteServico.java
+++ trunk/src/main/java/br/net/rwd/website/servico/SiteServico.java
@@ -1,41 +1,59 @@
 package br.net.rwd.website.servico;
 
+import java.io.Serializable;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.net.rwd.website.dao.DAOGenerico;
 import br.net.rwd.website.dao.SiteDAO;
 import br.net.rwd.website.entidade.Site;
 
 @Service("siteServico")
-public class SiteServico {
+public class SiteServico extends DAOGenerico<Serializable> {
 	
 	@Autowired
 	private SiteDAO dao;
-
-	public List<Site> getAllSites() {
-		return dao.findAll();
-	}
-
-	public List<Site> getSitesByName(String tituloSite) {
-		return dao.findByName(tituloSite);
-	}
-
-	public Site createSite(Site site) {
-		return dao.create(site);
-	}
-
-	public void updateSite(Site site) {
-		dao.update(site);
-	}
-
-	public void deleteSite(Site site) {
-		dao.delete(site);
-
-	}
-
-	public void setDao(SiteDAO dao) {
+	
+	protected void setDao(SiteDAO dao) {
 		this.dao = dao;
 	}
+	
+	public Site incluirSite(Site site) {
+		return dao.adicionar(site);
+	}
+	
+	public void editarSite(Site site) {
+		dao.atualizar(site);
+	}
+	
+	public void excluirSite(Site site) {
+		dao.remover(site);
+	}
+	
+	public Site selecionarSite() {
+		return dao.obterEntidade(Site.class, "SELECT s from Site s");
+	}
+	
+	public Site selecionarSite(int codigo) {
+		return dao.obterEntidade(Site.class, "SELECT s from Site s where s.web_cod = ?1", codigo);
+	}
+	
+	public Site selecionarSite(String nome) {
+		return dao.obterEntidade(Site.class, "SELECT s from Site s where s.web_titulo = ?1", nome);
+	}
+	
+	public List<Site> listarSite() {
+		return dao.obterLista(Site.class, "SELECT s from Site s ORDER BY s.web_cod ASC");
+	}
+	
+	public List<Site> listarSite(int codigo) {
+		return dao.obterLista(Site.class, "SELECT s from Site s where s.web_cod = ?1", codigo);
+	}
+	
+	public List<Site> listarSite(String nome) {
+		return dao.obterLista(Site.class, "SELECT s from Site s where s.web_titulo = ?1", nome);
+	}
+
 }
